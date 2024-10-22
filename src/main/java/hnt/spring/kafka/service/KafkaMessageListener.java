126c9ba9bd75f60ad3a031476506619085088242
HuyNT <tienhuylionel@gmail.com>
2024-10-22 17:50:33 +0700

    implement consumer

---
+++ src/main/java/hnt/spring/kafka/service/KafkaMessageListener.java
@@ -0,0 +1,27 @@
+package hnt.spring.kafka.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class KafkaMessageListener {
+    Logger logger = LoggerFactory.getLogger(KafkaMessageListener.class.getName());
+
+    @KafkaListener(topics = "kafka-topic-002", groupId = "group-1")
+    public void consume1(String message) {
+        logger.info("Consumer1 consume message {}", message);
+    }
+
+    @KafkaListener(topics = "kafka-topic-002", groupId = "group-1")
+    public void consume2(String message) {
+        logger.info("Consumer2 consume message {}", message);
+    }
+
+    @KafkaListener(topics = "kafka-topic-002", groupId = "group-1")
+    public void consume3(String message) {
+        logger.info("Consumer3 consume message {}", message);
+    }
+}
