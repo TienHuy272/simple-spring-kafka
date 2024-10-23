66cc9fe3e3f8b767e0df7d50277c594c50c137eb
HuyNT <tienhuylionel@gmail.com>
2024-10-23 10:01:48 +0700

    implement retrytopic and dead letter topic handler

--- src/main/java/hnt/spring/kafka/service/KafkaMessageListener.java
+++ src/main/java/hnt/spring/kafka/service/KafkaMessageListener.java
@@ -3,16 +3,16 @@ package hnt.spring.kafka.service;
 import hnt.spring.kafka.dto.Customer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
 import org.springframework.stereotype.Service;
-
-import java.util.function.Consumer;
 
 
 @Service
 public class KafkaMessageListener {
     Logger logger = LoggerFactory.getLogger(KafkaMessageListener.class.getName());
-
+    public static Integer retryCount = 0;
 //    @KafkaListener(topics = "kafka-topic-002", groupId = "group-1")
 //    public void consume1(String message) {
 //        logger.info("Consumer1 consume message {}", message);
@@ -28,8 +28,19 @@ public class KafkaMessageListener {
 //        logger.info("Consumer3 consume message {}", message);
 //    }
 
+    @RetryableTopic(attempts = "4")
     @KafkaListener(topics = "kafka-topic-003", groupId = "group-1")
     public void consume(Customer customer) {
-        logger.info("Consumer1 consume message {}", customer.toString());
+        logger.info("retry count : " + retryCount);
+        retryCount++;
+        if (customer.getName().equals("error")) {
+            throw new RuntimeException("Error");
+        }
+        logger.info("Consumer1 consume message {}", customer);
+    }
+
+    @DltHandler
+    public void listenDeadLetterTopic(Customer customer) {
+        logger.info("DTL receive message {}", customer);
     }
 }
